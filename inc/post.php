907116c36b3fafb1e89648b7e622b55ad0876339
Mark Lowery <mlow7920@gmail.com>
2017-10-19 18:29:43 -0400

    add ghealth to repository

--- inc/post.php
+++ inc/post.php
@@ -7,28 +7,44 @@
  */
 
 if(isset($_POST)){
-$authtoken  = $_POST['authtoken'];
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$phone = $_POST['phone'];
-$birth_date = $_POST['birth_date'];
-$insurance_carrier = $_POST['insurance_carrier'];
-$insurance_id = $_POST['insurance_id'];
+  $authtoken = 'authtoken : ghmark7920';
+$data = json_encode($_POST);
 
-$url = "https://apistage.gohealthuc.com:1981/v1/eligibility_demo/?authtoken=$authtoken";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST,true);
 
-$exec = curl_exec($ch);
-$info = curl_getinfo($ch);
-//print_r($info);
-curl_close($ch);
+$url = "https://apistage.gohealthuc.com:1981/v1/eligibility_demo";
 
-$json = json_decode($exec);
+try {
 
-print_r($json);
+  $ch = curl_init();
+  if (FALSE === $ch)
+    throw new Exception('failed to initialize');
+
+  curl_setopt($ch, CURLOPT_URL,$url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array($authtoken));
+ // curl_setopt( $ch, CURLOPT_ENCODING, "" );
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+ // curl_setopt($ch, CURLOPT_POST,true);
+ // curl_setopt($ch, CURLOPT_HEADER,false);
+
+  $exec = curl_exec($ch);
+ // $info = curl_getinfo($ch);
+  curl_close($ch);
+
+  header('Content-type: application/json');
+  echo $exec;
+
+
+} catch(Exception $e) {
+
+  trigger_error(sprintf(
+    'Curl failed with error #%d: %s',
+    $e->getCode(), $e->getMessage()),
+    E_USER_ERROR);
+
+}
 
 }
