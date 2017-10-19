c7599ef9e59edbd43fbe7ef964a433ebe2e07b83
Mark Lowery <mlow7920@gmail.com>
2017-10-19 18:29:17 -0400

    add ghealth to repository

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,22 @@ $(function () {
   var $last_name = $('#last_name');
   var $phone = $('#phone');
   var $birth_date = $('#datepicker');
+  var $pfirst_name = $('#pfirst_name');
+  var $plast_name = $('#plast_name');
   var $insurance_carrier = $('#insCarrier');
-  var $insurance_id = $('#insID');
+  var $patient_id = $('#insID');
 
   reg.reset();
 
   var rad =  $(this).attr("value");
   if (rad == "N") {
+
     $(".ins").hide('slow');
+
   }
 
   if (rad == "Y") {
+
     $(".ins").show('slow');
 
   }
@@ -23,41 +28,66 @@ $(function () {
   $('input[type="radio"]').click(function () {
 
     if ($(this).attr("value") == "N") {
+
       $(".ins").hide('slow');
+
     }
     if ($(this).attr("value") == "Y") {
+
       $(".ins").show('slow');
+
 
     }
   });
 
-  $('#datepicker').datepicker();
+  $('#datepicker').datepicker({ format: 'yyyy-mm-dd'});
 
 
-  $('#insID')
-      .focusout(function() {
+  $('#insID').focusout(function(e) {
 
-          var params = {
-          authtoken: '',
-          first_name : $first_name.val(),
-          last_name : $last_name.val(),
-          birth_date : $birth_date.val(),
-          phone : $phone.val(),
-          insurance_carrier : $insurance_carrier.val(),
-          insurance_id : $insurance_id.val()
+        e.preventDefault();
+          // phone: $phone.val()
+          var member = {
+            first_name: $first_name.val(),
+            last_name: $last_name.val(),
+            id : $patient_id.val(),
+            birth_date: $birth_date.val()
 
-        };
+          };
 
+          var provider = {
+            first_name: 'Marty',
+            last_name: 'Seeger',
+            npi: '1234567890'
 
+            };
+
+     var trading_partner_id = $insurance_carrier.val();
 
         $.ajax({
           type: "POST",
           url: "inc/post.php",
-          data: params,// now data come in this function
+          data: { member: member , provider:provider, trading_partner_id: trading_partner_id},
           cache: false,
-          success: function (data, status, jqXHR) {
- console.log(data);
-            //alert("success");// write success in " "
+          success: function (response, status, jqXHR) {
+                var d;
+
+
+                if(response.data != ""){
+                  d = response.data;
+                  console.log(d);
+                if(d.coverage.active){
+
+                 // if(d.coverage.copay[0].copayment.amount != "") d.coverage.copay[0].copayment.amount = '$0.00';
+
+                  $('#alert_msg').html('<div class="alert-success text-center"> <p> Your insurance is valid and your copay is ' + '$'+ d.coverage.copay[0].copayment.amount + '</p></div>');
+                } else {
+
+                  $('#alert_msg').html('<div class="alert-danger text-center"><p> Your insurance is invalid</p></div>');
+
+                }
+
+            }
           },
 
           error: function (jqXHR, status) {
@@ -68,7 +98,8 @@ $(function () {
         });
 
 
-      })
+      });
+
 
 });
 
